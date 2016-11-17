33ccf23e52e088765eac42ea0b38294f634cfec0
Aaron Opell <aaron.opell@live.com>
2016-11-17 10:59:40 -0800

    Update main.js

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var currentTime = new Date();
     var timeRemaining = getSecondsLeft();
     var currentPerson = people[getCurrentPerson()];
-    if (timeRemaining <= 1) {
+    if (timeRemaining < 1) {
         alertText = 'Switch! ' + people[getCurrentPerson() === 0 ? 1 : 0] + "'s turn";
         var notification = new Notification(alertText);
         setTimeout(() => {
